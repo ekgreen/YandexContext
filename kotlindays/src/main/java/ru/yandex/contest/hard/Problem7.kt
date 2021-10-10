e12f9e9e3dfb14bc232d4064bee7e4821c5d7903
Дмитрий l W.W <shigalkin@mail.ru>
2021-10-10 15:02:31 +0300

    problem7

--- kotlindays/src/main/java/ru/yandex/contest/hard/Problem7.kt
+++ kotlindays/src/main/java/ru/yandex/contest/hard/Problem7.kt
@@ -4,17 +4,69 @@ import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.util.*
+import kotlin.math.min
 
 fun main(){
     val reader: Problem7FastInputReader = Problem7FastInputReader()
 
-    val N: Int = reader.nextInt()
+    val a = reader.nextLine().toCharArray()
+    val b = reader.nextLine().toCharArray()
 
-    println(problem7())
+    println(editDistance(a, b))
 }
 
-fun problem7(){
-    TODO()
+/**
+ * Вычисление минимального расстояния между строками
+ *
+ * A - первая строка
+ * B - вторая строка
+ *
+ * insertion = вставка в первую строку A    = +1
+ * deletion  = вставка во вторую строку B   = +1
+ * match     = A[i] == B[j]                 = +0
+ * mismatch  = A[i] != B[j]                 = +1
+ *
+ * Example comparing:
+ *
+ * A[1....n] = EDITING
+ * B[1....m] = DISTANCE
+ *
+ * @see <a href="notes/edit.distance.algorighm.png"></a>
+ */
+fun editDistance(A: CharArray, B: CharArray): Int {
+    return distanceArray(A,B)[A.size][B.size]
+}
+
+fun distanceArray(A: CharArray, B: CharArray): Array<IntArray>{
+    fun min(insertion: Int, deletion: Int, mathOrMismatch: Int): Int {
+        return min(min(insertion, deletion), mathOrMismatch)
+    }
+
+    val n = A.size + 1
+    val m = B.size + 1
+
+    val D: Array<IntArray> = Array(n) { IntArray(m) }
+
+    for (i in 0 until n)
+        D[i][0] = i // i - строки
+    for (j in 0 until m)
+        D[0][j] = j // j - столбцы
+
+    for (j in 1 until m) {
+        for (i in 1 until n) {
+            val insertion = D[i][j - 1] + 1
+            val deletion = D[i - 1][j] + 1
+            val match = D[i - 1][j - 1]
+            val mismatch = D[i - 1][j - 1] + 1
+
+            if (A[i - 1] == B[j - 1])
+                D[i][j] = min(insertion, deletion, match)
+            else
+                D[i][j] = min(insertion, deletion, mismatch)
+        }
+    }
+
+    return D
 }
 
 internal class Problem7FastInputReader(inputStream: InputStream = System.`in`) {
--- kotlindays/src/test/java/ru/yandex/contest/hard/Problem7KtTest.kt
+++ kotlindays/src/test/java/ru/yandex/contest/hard/Problem7KtTest.kt
@@ -1,72 +1,72 @@
-package ru.yandex.contest.hard
-
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-import problem7
-
-internal class Problem7KtTest{
-
-    @Test
-    fun `problem7 test case #1`(){
-        problem7()
-
-        Assertions.assertEquals(1,1)
-    }
-
-    @Test
-    fun `problem7 test case #2`(){
-        problem7()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem7 test case #3`(){
-        problem7()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem7 test case #4`(){
-        problem7()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem7 test case #5`(){
-        problem7()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem7 test case #6`(){
-        problem7()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem7 test case #7`(){
-        problem7()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem7 test case #8`(){
-        problem7()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem7 test case #9`(){
-        problem7()
-
-//        Assertions.assertEquals(,)
-    }
-
-}
+//package ru.yandex.contest.hard
+//
+//import org.junit.jupiter.api.Assertions
+//import org.junit.jupiter.api.Test
+//import problem7
+//
+//internal class Problem7KtTest{
+//
+//    @Test
+//    fun `problem7 test case #1`(){
+//        problem7()
+//
+//        Assertions.assertEquals(1,1)
+//    }
+//
+//    @Test
+//    fun `problem7 test case #2`(){
+//        problem7()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem7 test case #3`(){
+//        problem7()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem7 test case #4`(){
+//        problem7()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem7 test case #5`(){
+//        problem7()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem7 test case #6`(){
+//        problem7()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem7 test case #7`(){
+//        problem7()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem7 test case #8`(){
+//        problem7()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem7 test case #9`(){
+//        problem7()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//}
