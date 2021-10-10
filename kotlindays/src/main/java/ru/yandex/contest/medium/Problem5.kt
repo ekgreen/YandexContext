53fb23856094c1a68f2d8893f12d2c23f2bce656
Danil Chernykh <degget.dufus@gmail.com>
2021-10-10 13:36:06 +0300

    problem5

--- kotlindays/src/main/java/ru/yandex/contest/medium/Problem5.kt
+++ kotlindays/src/main/java/ru/yandex/contest/medium/Problem5.kt
@@ -4,16 +4,71 @@ import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.util.*
+import java.util.ArrayList
 
-fun main(){
-    val reader: Problem5FastInputReader = Problem5FastInputReader()
+import java.util.Arrays
+import java.util.HashSet
 
-    val N: Int = reader.nextInt()
+fun main() {
+    val reader: Problem7FastInputReader = Problem7FastInputReader()
 
-    println(problem5())
+    println(threeSumDanya(reader.nextLine().removeSurrounding("[", "]").split(", ").map { it.toInt() }.toIntArray()))
+    println(threeSumRoma(reader.nextLine().removeSurrounding("[", "]").split(", ").map { it.toInt() }.toIntArray()))
+
 }
 
-fun problem5(){
+fun threeSumDanya(input: IntArray): String {
+    val result: MutableList<List<Int>> = ArrayList()
+    Arrays.sort(input)
+    val n = input.size
+    var a = 0
+    while (a < n) {
+        var b = a + 1
+        var c = n - 1
+        while (b < c) {
+            if (input[b] + input[c] == -input[a]) {
+                val temp: MutableList<Int> = ArrayList()
+                temp.add(input[a])
+                temp.add(input[b])
+                temp.add(input[c])
+                result.add(temp)
+                while (b + 1 < c && input[b + 1] == input[b]) b++
+                while (c - 1 > b && input[c - 1] == input[c]) c--
+                b++
+                c--
+            } else if (input[b] + input[c] < -input[a]) b++ else c--
+        }
+        while (a + 1 < n && input[a + 1] == input[a]) a++
+        a++
+    }
+    return result.joinToString(", ", "[", "]")
+}
+
+
+fun threeSumRoma(array: IntArray): String {
+    // Write your code here.
+    val targetSum = 0
+    Arrays.sort(array)
+    val result = ArrayList<Array<Int>>()
+    val distinct: MutableSet<Int> = HashSet()
+    for (value in array) {
+        distinct.add(value)
+    }
+    for (v in array.indices) {
+        val c1 = array[v]
+        val c2c3 = targetSum - c1
+        for (v2 in v + 1 until array.size) {
+            val c2 = array[v2]
+            val c3 = c2c3 - c2
+            if (c2 in (c1 + 1) until c3 && distinct.contains(c3)) {
+                result.add(arrayOf(c1, c2, c3))
+            }
+        }
+    }
+    return "[${result[0].joinToString(", ", "[", "]")}, ${result[1].joinToString(", ", "[", "]")}]"
+}
+
+fun problem5() {
     TODO()
 }
 
@@ -21,7 +76,7 @@ private class Problem5FastInputReader(inputStream: InputStream = System.`in`) {
     private val reader: BufferedReader = BufferedReader(InputStreamReader(inputStream))
     private var tokenizer: StringTokenizer? = null
 
-    fun next(): String{
+    fun next(): String {
         while (tokenizer == null || !tokenizer!!.hasMoreElements()) tokenizer = StringTokenizer(nextLine())
         return tokenizer!!.nextToken()
     }
@@ -42,3 +97,4 @@ private class Problem5FastInputReader(inputStream: InputStream = System.`in`) {
         return reader.readLine()
     }
 }
+
