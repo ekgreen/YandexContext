3826836e027a57207a7654b00607ff9205743e3e
Дмитрий l W.W <shigalkin@mail.ru>
2021-10-10 15:08:04 +0300

    problem8

--- kotlindays/src/main/java/ru/yandex/contest/hard/Problem8.kt
+++ kotlindays/src/main/java/ru/yandex/contest/hard/Problem8.kt
@@ -8,13 +8,40 @@ import java.util.*
 fun main(){
     val reader: Problem8FastInputReader = Problem8FastInputReader()
 
-    val N: Int = reader.nextInt()
+    var array = reader.nextLine().removeSurrounding("[", "]").split(",").map { it.toInt() }.toMutableList()
 
-    println(problem8())
+    println(problem8(array))
 }
 
-fun problem8(){
-    TODO()
+fun problem8(array: MutableList<Int>): Int {
+    for (i in array.indices) {
+        if (array[i] % 2 != 0) {
+            array[i] *= 2
+        }
+    }
+
+    var flag = true
+    var diff = Integer.MAX_VALUE
+
+    var max: Int
+    var min: Int
+
+    while (flag) {
+        max = array.maxOrNull()!!
+        min = array.minOrNull()!!
+
+        if (max - min < diff) {
+            diff = max - min
+        }
+
+        if (max % 2 == 0) {
+            array[array.indexOf(max)] = max / 2
+        } else {
+            flag = false
+        }
+    }
+
+    return diff
 }
 
 private class Problem8FastInputReader(inputStream: InputStream = System.`in`) {
--- kotlindays/src/test/java/ru/yandex/contest/hard/Problem8KtTest.kt
+++ kotlindays/src/test/java/ru/yandex/contest/hard/Problem8KtTest.kt
@@ -1,72 +1,72 @@
-package ru.yandex.contest.hard
-
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-import problem8
-
-internal class Problem8KtTest{
-
-    @Test
-    fun `problem8 test case #1`(){
-        problem8()
-
-        Assertions.assertEquals(1,1)
-    }
-
-    @Test
-    fun `problem8 test case #2`(){
-        problem8()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem8 test case #3`(){
-        problem8()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem8 test case #4`(){
-        problem8()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem8 test case #5`(){
-        problem8()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem8 test case #6`(){
-        problem8()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem8 test case #7`(){
-        problem8()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem8 test case #8`(){
-        problem8()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem8 test case #9`(){
-        problem8()
-
-//        Assertions.assertEquals(,)
-    }
-
-}
+//package ru.yandex.contest.hard
+//
+//import org.junit.jupiter.api.Assertions
+//import org.junit.jupiter.api.Test
+//import problem8
+//
+//internal class Problem8KtTest{
+//
+//    @Test
+//    fun `problem8 test case #1`(){
+//        problem8()
+//
+//        Assertions.assertEquals(1,1)
+//    }
+//
+//    @Test
+//    fun `problem8 test case #2`(){
+//        problem8()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem8 test case #3`(){
+//        problem8()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem8 test case #4`(){
+//        problem8()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem8 test case #5`(){
+//        problem8()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem8 test case #6`(){
+//        problem8()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem8 test case #7`(){
+//        problem8()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem8 test case #8`(){
+//        problem8()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem8 test case #9`(){
+//        problem8()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//}
