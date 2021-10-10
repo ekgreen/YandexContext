fd6b63966d244aaf52f043fdf060a4fb613ccbaf
Дмитрий l W.W <shigalkin@mail.ru>
2021-10-10 11:49:53 +0300

    problemA

--- kotlindays/src/main/java/ru/yandex/contest/easy/Problem1.kt
+++ kotlindays/src/main/java/ru/yandex/contest/easy/Problem1.kt
@@ -1,4 +1,4 @@
-//package ru.yandex.contest.easy
+package ru.yandex.contest.easy
 
 import java.io.BufferedReader
 import java.io.InputStream
@@ -8,13 +8,22 @@ import java.util.*
 fun main(){
     val reader: Problem1FastInputReader = Problem1FastInputReader()
 
-    val N: Int = reader.nextInt()
+    var list = reader.nextLine().removeSurrounding("[", "]").split(", ").map { it.toInt() }.toList()
 
-    println(problem1())
+    println(problem1(list))
 }
 
-fun problem1(){
-    TODO()
+fun problem1(array: List<Int>): Int {
+    val n = array.size
+
+    var sum = 0
+    for (i in array.indices) {
+        sum += array[i]
+    }
+
+    val total: Int = (array.size + 1) * array.size / 2
+
+    return total - sum
 }
 
 internal class Problem1FastInputReader(inputStream: InputStream = System.`in`) {
--- kotlindays/src/test/java/ru/yandex/contest/easy/Problem1KtTest.kt
+++ kotlindays/src/test/java/ru/yandex/contest/easy/Problem1KtTest.kt
@@ -1,72 +1,71 @@
-package ru.yandex.contest.easy
-
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-import problem1
-
-internal class Problem1KtTest{
-
-    @Test
-    fun `problem1 test case #1`(){
-        problem1()
-
-        Assertions.assertEquals(1,1)
-    }
-
-    @Test
-    fun `problem1 test case #2`(){
-        problem1()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem1 test case #3`(){
-        problem1()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem1 test case #4`(){
-        problem1()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem1 test case #5`(){
-        problem1()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem1 test case #6`(){
-        problem1()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem1 test case #7`(){
-        problem1()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem1 test case #8`(){
-        problem1()
-
-//        Assertions.assertEquals(,)
-    }
-
-    @Test
-    fun `problem1 test case #9`(){
-        problem1()
-
-//        Assertions.assertEquals(,)
-    }
-
-}
+//package ru.yandex.contest.easy
+//
+//import org.junit.jupiter.api.Assertions
+//import org.junit.jupiter.api.Test
+//
+//internal class Problem1KtTest{
+//
+//    @Test
+//    fun `problem1 test case #1`(){
+//        problem1()
+//
+//        Assertions.assertEquals(1,1)
+//    }
+//
+//    @Test
+//    fun `problem1 test case #2`(){
+//        problem1()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem1 test case #3`(){
+//        problem1()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem1 test case #4`(){
+//        problem1()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem1 test case #5`(){
+//        problem1()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem1 test case #6`(){
+//        problem1()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem1 test case #7`(){
+//        problem1()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem1 test case #8`(){
+//        problem1()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//    @Test
+//    fun `problem1 test case #9`(){
+//        problem1()
+//
+////        Assertions.assertEquals(,)
+//    }
+//
+//}
