62198972754f887989a4f5314b4cc0bb3a8af5fb
Danil Chernykh <degget.dufus@gmail.com>
2021-10-10 13:52:33 +0300

    problem5(2)

--- kotlindays/src/main/java/ru/yandex/contest/medium/Problem5.kt
+++ kotlindays/src/main/java/ru/yandex/contest/medium/Problem5.kt
@@ -5,23 +5,28 @@ import java.io.InputStream
 import java.io.InputStreamReader
 import java.util.*
 import java.util.ArrayList
-
 import java.util.Arrays
-import java.util.HashSet
+
 
 fun main() {
     val reader: Problem7FastInputReader = Problem7FastInputReader()
 
     println(threeSumDanya(reader.nextLine().removeSurrounding("[", "]").split(", ").map { it.toInt() }.toIntArray()))
-    println(threeSumRoma(reader.nextLine().removeSurrounding("[", "]").split(", ").map { it.toInt() }.toIntArray()))
-
 }
 
 fun threeSumDanya(input: IntArray): String {
-    val result: MutableList<List<Int>> = ArrayList()
+//    if (input.isEmpty()){
+//        return "[]"
+//    }
+    if (input.size == 1){
+        return "[${input[0]}]"
+    }
+
     Arrays.sort(input)
+    val result: MutableList<List<Int>> = ArrayList()
     val n = input.size
     var a = 0
+
     while (a < n) {
         var b = a + 1
         var c = n - 1
@@ -41,32 +46,10 @@ fun threeSumDanya(input: IntArray): String {
         while (a + 1 < n && input[a + 1] == input[a]) a++
         a++
     }
+
     return result.joinToString(", ", "[", "]")
 }
 
-
-fun threeSumRoma(array: IntArray): String {
-    // Write your code here.
-    val targetSum = 0
-    Arrays.sort(array)
-    val result = ArrayList<Array<Int>>()
-    val distinct: MutableSet<Int> = HashSet()
-    for (value in array) {
-        distinct.add(value)
-    }
-    for (v in array.indices) {
-        val c1 = array[v]
-        val c2c3 = targetSum - c1
-        for (v2 in v + 1 until array.size) {
-            val c2 = array[v2]
-            val c3 = c2c3 - c2
-            if (c2 in (c1 + 1) until c3 && distinct.contains(c3)) {
-                result.add(arrayOf(c1, c2, c3))
-            }
-        }
-    }
-    return "[${result[0].joinToString(", ", "[", "]")}, ${result[1].joinToString(", ", "[", "]")}]"
-}
 
 fun problem5() {
     TODO()
