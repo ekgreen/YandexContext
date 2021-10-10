08af4068fe6eb444b7d0240301b82927df782209
Elion Green <rgizmalkov.dev@gmail.com>
2021-10-10 14:02:02 +0300

    Задание 5

--- kotlindays/src/main/java/ru/yandex/contest/medium/Problem5.kt
+++ kotlindays/src/main/java/ru/yandex/contest/medium/Problem5.kt
@@ -7,20 +7,22 @@ import java.util.*
 import java.util.ArrayList
 import java.util.Arrays
 
-
 fun main() {
     val reader: Problem7FastInputReader = Problem7FastInputReader()
 
-    println(threeSumDanya(reader.nextLine().removeSurrounding("[", "]").split(", ").map { it.toInt() }.toIntArray()))
+    val line: String = reader.nextLine().removeSurrounding("[", "]")
+
+    println(threeSumDanya(line))
 }
 
-fun threeSumDanya(input: IntArray): String {
-//    if (input.isEmpty()){
-//        return "[]"
-//    }
-    if (input.size == 1){
-        return "[${input[0]}]"
-    }
+fun threeSumDanya(line: String): String {
+    if(line.isEmpty())
+        return "[]"
+
+    val input: IntArray = line.split(", ").map { it.toInt() }.toIntArray()
+
+    if (input.size < 3)
+        return "[]"
 
     Arrays.sort(input)
     val result: MutableList<List<Int>> = ArrayList()
@@ -30,29 +32,24 @@ fun threeSumDanya(input: IntArray): String {
     while (a < n) {
         var b = a + 1
         var c = n - 1
+
         while (b < c) {
             if (input[b] + input[c] == -input[a]) {
-                val temp: MutableList<Int> = ArrayList()
-                temp.add(input[a])
-                temp.add(input[b])
-                temp.add(input[c])
-                result.add(temp)
+                result.add(listOf(input[a],input[b],input[c]))
+
                 while (b + 1 < c && input[b + 1] == input[b]) b++
                 while (c - 1 > b && input[c - 1] == input[c]) c--
+
                 b++
                 c--
             } else if (input[b] + input[c] < -input[a]) b++ else c--
         }
+
         while (a + 1 < n && input[a + 1] == input[a]) a++
         a++
     }
 
     return result.joinToString(", ", "[", "]")
-}
-
-
-fun problem5() {
-    TODO()
 }
 
 private class Problem5FastInputReader(inputStream: InputStream = System.`in`) {
