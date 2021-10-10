9bd8de3bfab2fe71deb7e8d9a84fd58865234a7c
Elion Green <rgizmalkov.dev@gmail.com>
2021-10-10 19:17:14 +0300

    Решение 4

--- kotlindays/src/main/java/ru/yandex/contest/medium/Problem4.kt
+++ kotlindays/src/main/java/ru/yandex/contest/medium/Problem4.kt
@@ -3,9 +3,7 @@
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.lang.StringBuilder
 import java.util.*
-import kotlin.collections.ArrayList
 
 fun main() {
     val reader: Problem4FastInputReader = Problem4FastInputReader()
@@ -16,45 +14,29 @@ fun main() {
 }
 
 fun problem4(S: String): String {
-    val result: MutableList<Pair<Char, Int>> = ArrayList()
+    val set: MutableSet<Char> = HashSet<Char>()
+    val stack: Stack<Char> = Stack<Char>()
+    val array = IntArray(26)
 
-    val chars: MutableList<Pair<Char, Int>> = ArrayList()
-    for (i in S.indices)
-        chars.add(Pair(S[i], i))
-    chars.sortWith { o1, o2 -> o1.first.compareTo(o2.first) }
+    for (i in S)
+        array[i - 'a'] += 1
 
-    var i = 0
-
-    while (i < chars.size) { // a - 0, b - 7, c - ?
-        var pair = chars[i]
-
-        var j = result.size
-        while (i < chars.size && j >= 0) {
-            val position: Int = if (j > 0) result[result.size - 1].second else -1
-            val char = chars[i]
-
-            if(position == -1){
-                while (++i< chars.size && char.first == chars[i].first) {/*SKIP*/}
-                break;
-            } else if (char.second > position) {
-                while (++i < chars.size && char.first == chars[i].first) {/*SKIP*/}
-                pair = char
-                break;
-            } else {
-                j -= 1
-            }
+    for (ch in S) {
+        if (set.contains(ch)) {
+            array[ch - 'a'] -= 1
+            continue
         }
 
-        result.add(pair)
-        result.sortWith { o1, o2 -> o1.second.compareTo(o2.second) }
+        while (!stack.isEmpty() && stack.peek() > ch && array[stack.peek() - 'a'] > 0) {
+            set.remove(stack.pop())
+        }
+
+        stack.add(ch)
+        array[ch - 'a'] -= 1
+        set.add(ch)
     }
 
-    result.sortWith { o1, o2 -> o1.second.compareTo(o2.second) }
-    val builder: StringBuilder = StringBuilder()
-    for (k in result.indices)
-        builder.append(result[k].first)
-
-    return builder.toString()
+    return stack.joinToString("")
 }
 
 internal class Problem4FastInputReader(inputStream: InputStream = System.`in`) {
